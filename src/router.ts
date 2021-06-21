ef9df15c098acefb1f89cf443a81428daa3e81d9
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-06-21 16:13:23 -0300

    Criado o esqueleto do controller do banco de dados

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,15 @@ import express from 'express'
 import RouterNotes from "./controllers/Notes.controller"
 const router = express.Router()
 
+
+// Notes
 router.patch('/notes/edit', RouterNotes.edit)
 router.get('/notes/list', RouterNotes.search)
 router.post('/notes/new', RouterNotes.create)
 router.delete('/notes/delete', RouterNotes.delete)
+
+// Users
+    // In progress
 
 export default router
 
---
+++ src/utils/DatabaseControl.ts
@@ -0,0 +1,25 @@
+import db from '../config/database'
+
+export default class DatabaseControl {
+
+    public static create(){
+
+    }
+
+    public static delete(){
+
+    }
+
+    public static edit(){
+
+    }
+
+    public static find(){
+
+    }
+
+    public static findAll(){
+        
+    }
+
+}
