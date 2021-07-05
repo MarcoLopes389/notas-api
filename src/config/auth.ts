cbea113e42c5854042dd84c9b85ba75dcdbd652c
Marco Lopes <marco.lopes.araujo@gmail.com>
2021-07-05 00:48:10 -0300

    Colocando id nas variáveis globais

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -5,7 +5,8 @@ export async function Auth (req: Request, res: Response) {
 
     const token = req.headers.authorization?.replace('Bearer', '')
     try {
-        await JWTservice.verify(token || '')
+        const decoded: any = await JWTservice.verify(token || '')
+        res.locals.user = decoded.id
 
     } catch {
         res.sendStatus(401)
